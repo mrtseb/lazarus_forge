3efb32d2e60508c2957d74704644c394ab2df7b1
mistert14 <sebastien.tack@sfr.fr>
2017-10-25 14:42:18 +0200

    strip exe files

---
+++ dbf1/clean.bat
@@ -0,0 +1,11 @@
+del /s /q *.~*
+del /s /q *.raw
+del /s /q *.cir
+del /s /q *.dcu
+del /s /q *.log
+del /s /q *.ppu
+del /s /q *.bak
+del /s /q *.obj
+del /q *.o
+strip.exe TechnoComp.exe
+strip.exe Project1.exe
---
+++ firmata_ctl/clean.bat
@@ -0,0 +1,4 @@
+del /s /q *.~*
+del /s /q *.bak
+strip.exe project1.exe
+
